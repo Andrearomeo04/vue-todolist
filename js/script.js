6f9a876975d73413915e71d586987380e808c9d2
andrearomeo04 <pisco004@gmail.com>
2024-07-19 02:42:37 +0200

    [UPD] created an array of objects and cicled it in the dom

--- js/script.js
+++ js/script.js
@@ -3,7 +3,36 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-
+            todolist: [
+                { 
+                  text: 'Fare colazione', 
+                  done: false 
+                },
+                { 
+                  text: 'Andare a lavoro', 
+                  done: false 
+                },
+                { 
+                  text: 'Fare la spesa', 
+                  done: false 
+                },
+                { 
+                  text: 'Preparare il pranzo', 
+                  done: false 
+                },
+                { 
+                  text: 'Fare esercizio fisico', 
+                  done: false 
+                },
+                { 
+                  text: 'Controllare le email', 
+                  done: false 
+                },
+                { 
+                  text: 'Preparare la cena', 
+                  done: false 
+                }
+              ]
         }
     }
 }).mount('#app')

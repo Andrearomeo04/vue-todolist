cf7b23796254d6b088bac3a864dc29f2f5d3149e
andrearomeo04 <pisco004@gmail.com>
2024-07-19 04:26:22 +0200

    [UPD] milestone 2 completata

--- js/script.js
+++ js/script.js
@@ -33,20 +33,12 @@ createApp({
                   done: false 
                 }
               ],
-              newtask: null
         }
     },
     methods: {
         deletetask (index) {
             if(confirm('ATTENZIONE, STAI PER CANCELLARE QUESTA TASK, QUESTA AZIONE è IRREVERSIBILE. SEI SICURO DI VOLER CONTINUARE?'))
                 this.todolist.splice(index, 1)
-        },
-        addtask () {
-            this.todolist.push({
-                text: this.newtask,
-                done: false
-            })
-            this.newtask = ''
         },
         checktask (index) {
             this.todolist[index].done = !this.todolist[index].done
